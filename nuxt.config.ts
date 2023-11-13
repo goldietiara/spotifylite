8e0f34d4a7757c145f9e85daf8308ef089f69b1a
owy <126146587+goldietiara@users.noreply.github.com>
2023-11-13 19:44:45 +0700

    feat: auth with github
    login with github :D

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/ui", "nuxt-primevue", "@pinia/nuxt"],
+  modules: ["@nuxt/ui", "nuxt-primevue", "@pinia/nuxt", "@nuxtjs/supabase"],
   css: [
     "~/assets/css/main.css",
     "primevue/resources/themes/lara-light-teal/theme.css",
   ],
+  runtimeConfig: {
+    public: {
+      bucketUrl: process.env.BUCKETURL,
+    },
+  },
   primevue: {
     options: {
       ripple: true,

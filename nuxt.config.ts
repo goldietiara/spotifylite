070eea05adc0042e290b1413ecee57f99855464e
owy <126146587+goldietiara@users.noreply.github.com>
2024-05-17 22:00:34 +0700

    fix bug 3

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,6 +21,8 @@ export default defineNuxtConfig({
     cssLayerOrder: "tailwind-base, primevue, tailwind-utilities",
     components: {
       // prefix: "Prime",
+      exclude: ["Editor", "Chart"],
+
       include: "*",
     },
   },
